a098075b758a23725cfe1b96c881c42477f32211
gonzalo <gonzalosuar03@hotmail.com>
2023-10-23 19:09:44 -0300

    act

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 let icono = document.querySelector(".menu__icon");
 let contenedor = document.querySelector(".navbar");
+let enlaceAncla = document.querySelectorAll(".navbar a");
 
 
 icono.addEventListener("click",()=>
 {
     contenedor.classList.toggle("show");
 })
+
+enlaceAncla.forEach(enlace=>
+    {
+        enlace.addEventListener("click",()=>
+        {
+            contenedor.classList.remove("show");
+        })
+    })
